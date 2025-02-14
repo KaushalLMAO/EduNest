6438d0f78e1f7686fecd377ae18c51cae0239617
KaushalLMAO <gautamkaushal381@gmail.com>
2025-02-14 07:19:07 +0545

    fixes

--- settings.js
+++ settings.js
@@ -17,6 +17,7 @@ signout.addEventListener("click", (e) => {
   if (localStorage.getItem("email") === null) {
     alert("Please login first");
   } else {
+    confirm("Are you sure you want to logout?");
     window.location.href = "login.html";
   }
 });
@@ -40,8 +41,23 @@ if (profilesettings) {
             <input type="text" id="name" />
             <input type="email" id="email" />
             <input type="password" id="password" />
-            <input type="file" id="profile" />
-          </section>`;
+            <input type="file" id="profile" accept="image/*" />
+          </section>
+          <img src="default.png" id="preview" />
+        </div>
+        <div id="buttons">
+          <button type="button" id="save">Save Changes</button>
+          <button type="button" id="cancel">Cancel</button>
+          <button type="button" id="show">Preview Profile</button>
+        </div>`;
     }
   });
 }
+let prevprofile = document.getElementById("show");
+prevprofile.addEventListener("click", () => {
+  alert("You cannot perform this at the moment");
+});
+let save = document.getElementById("save");
+save.addEventListener("click", () => {
+  alert("You cannot perform this at the moment");
+});
